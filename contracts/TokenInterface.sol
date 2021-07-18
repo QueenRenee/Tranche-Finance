5634c76895ed0675ab9bfe59d2b4b0bdcd62f5ea
Reneevh <vanheerdenrenee@gmail.com>
2021-07-18 20:33:12 +0400

    Readme file edit start

--- contracts/TokenInterface.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
-
-abstract contract TokenInterface {
-    function deposit() external virtual payable;
-    function withdraw(uint256) external virtual;
-}
--- contracts/interfaces/IWETHGateway.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
-
-interface IWETHGateway {
-  function depositETH() external payable;
-  function withdrawETH(uint256 amount) external;
-}
